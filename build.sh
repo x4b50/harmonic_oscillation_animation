a121e71d7ebf13798cdc814852d4e7f00a0970bc
x4b50 <k.piwowarski@proton.me>
2024-03-02 19:37:24 +0100

    basics of reactive ui

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 # https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux
-gcc main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o main &&
-    ./main
+gcc main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o main
--- main.c
+++ main.c
@@ -1,16 +1,51 @@
 #include "raylib.h"
 
+#define WINDOW_W 1280
+#define WINDOW_H 720
+#define UNIT_RADUIS 1/8
+#define PADDING 1/30
+#define SPRING_W 1/8
+
+#define COLOR(c) *(Color*)&(c)
+int BG_COLOR = 0xFF181818;
+
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    // anti-alyasing
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(WINDOW_W, WINDOW_H, "raylib [core] example - basic window");
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        int window_w = GetScreenWidth();
+        int window_h = GetScreenHeight();
+
+        int padding = window_w*PADDING;
+        int u_raduis = window_w*UNIT_RADUIS;
+        if (u_raduis > (window_h-padding)/2)
+            u_raduis = (window_h-padding)/2;
+        int spring_w = window_w*SPRING_W;
+        int rect_h = u_raduis*2;
+        int rect_w = window_w -(u_raduis*2+spring_w+padding*4);
+
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(COLOR(BG_COLOR));
+        DrawCircle(padding+u_raduis, window_h/2, u_raduis, LIGHTGRAY);
+        DrawRectangleLines((padding+u_raduis)*2, padding, spring_w, window_h-2*padding, LIGHTGRAY);
+        DrawRectangle(
+                u_raduis*2+spring_w+padding*3,
+                window_h/2-rect_h/2,
+                rect_w, rect_h,
+                LIGHTGRAY);
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
 }
+
+
+/*
+    o | []
+*/
